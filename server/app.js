ae5641d04de59508f2af702749cd3f99e29df8ff
Akhilesh N Yadav <akki.592.148@Akkis-MacBook-Pro.local>
2026-02-22 00:05:49 +0530

    chores: fe build

--- server/app.js
+++ server/app.js
@@ -14,18 +14,12 @@ const app = express();
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "PUT", "POST", "PATCH", "DELETE"],
-    credentials: true,
-  }),
-);
+app.use(cors());
 app.use(cookieParser());
 
-app.use("/posts", postRoutes);
-app.use("/auth", authRouter);
-app.use("/user", userRouter);
+app.use("/api/posts", postRoutes);
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
 mongoose
   .connect(process.env.MONGO_URI, {
